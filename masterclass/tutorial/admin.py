c55233318ee793309bb79f1bb60167f135cf1d25
PolinaTurina <polinaturina87@gmail.com>
2024-04-06 18:50:41 +0500

    'фильтрация_по_категориям'

--- masterclass/tutorial/admin.py
+++ masterclass/tutorial/admin.py
@@ -7,3 +7,13 @@ from . models import *
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title']
     search_fields = ['title']
+
+@admin.register(Locate)
+class LocateAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    search_fields = ['title']
+
+@admin.register(MasterClass)
+class MasterClassAdmin(admin.ModelAdmin):
+    list_display = ['category', 'title', 'description', 'time', 'locate', 'price', 'author', 'photo']
+    search_fields = ['category', 'locate', 'price', 'author']
