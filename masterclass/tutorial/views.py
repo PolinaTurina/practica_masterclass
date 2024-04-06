154f7618c7ed41653128298f469a2103be7c3e92
PolinaTurina <polinaturina87@gmail.com>
2024-04-06 20:13:06 +0500

    'переход_на_детали_мастерклассов'

--- masterclass/tutorial/views.py
+++ masterclass/tutorial/views.py
@@ -7,15 +7,26 @@ from . models import *
 #     template_name = 'tutorial/full.html'
 #     model = Category
 #     context_object_name = 'category_list'
+
+
+# вывод главной страницы с категориями
 def tutorial_view(request):
     category = Category.objects.all()
     masterclass = MasterClass.objects.all()
     context = {'category': category, 'masterclass': masterclass}
     return render(request, 'tutorial/full.html', context)
 
+
+# фильтрация мастер классов по категорями на этой же главной странице
 def tutorial_list(request, pk):
     category = Category.objects.all()
     masterclass = MasterClass.objects.filter(category__pk=pk)
     context = {'category': category, 'masterclass': masterclass}
     return render(request, 'tutorial/full.html', context)
 
+
+# переход на детали мастеркласса
+def tutorial_detail(request, pk):
+    masterclass = MasterClass.objects.get(pk=pk)
+    context = {'masterclass': masterclass}
+    return render(request, 'tutorial/detail.html', context)
